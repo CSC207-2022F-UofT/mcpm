49242d20b3ebfad28db6961bf42c16978d03504c
Hykilpikonna <me@hydev.org>
2022-10-30 01:14:11 -0400

    [+] Interface ReloadBoundary

---
+++ src/main/java/org/hydev/mcpm/client/injector/ReloadBoundary.java
@@ -0,0 +1,17 @@
+package org.hydev.mcpm.client.injector;
+
+/**
+ * Interface for reload a locally installed plugin.
+ *
+ * @author Azalea (https://github.com/hykilpikonna)
+ * @since 2022-10-29
+ */
+public interface ReloadBoundary
+{
+    /**
+     * Unload then load a plugin
+     *
+     * @param name Loaded plugin name
+     */
+    public void reloadPlugin(String name);
+}
