a974045e329381b7aae8537afba78743800bd289
Hykilpikonna <me@hydev.org>
2022-10-29 23:25:26 -0400

    [T] Test setPrivateField()

--- src/test/java/org/hydev/mcpm/utils/ReflectionUtilsTest.java
+++ src/test/java/org/hydev/mcpm/utils/ReflectionUtilsTest.java
@@ -3,6 +3,7 @@ package org.hydev.mcpm.utils;
 import com.google.common.reflect.TypeToken;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -19,7 +20,8 @@ class ReflectionUtilsTest
 {
     private static class A
     {
-        private final List<String> field = Arrays.asList("meow", "qwq");
+        @SuppressWarnings({"unused", "MismatchedQueryAndUpdateOfCollection"})
+        private final List<String> field = new ArrayList<>(List.of("meow", "qwq"));
     }
 
     @Test
@@ -34,5 +36,18 @@ class ReflectionUtilsTest
         f.set(1, "wolf");
         var newF = ReflectionUtils.getPrivateField(a, "field", new TypeToken<List<String>>(){}).orElseThrow();
         assert newF.get(1).equals("wolf");
+    }
+
+    @Test
+    void setPrivateField()
+    {
+        var a = new A();
+        var f = ReflectionUtils.getPrivateField(a, "field", new TypeToken<List<String>>(){}).orElseThrow();
+        assert f.get(0).equals("meow");
+        assert ReflectionUtils.setPrivateField(a, "field", List.of("wolf"));
+        assert f.get(0).equals("meow");
+
+        var newF = ReflectionUtils.getPrivateField(a, "field", new TypeToken<List<String>>(){}).orElseThrow();
+        assert newF.get(0).equals("wolf");
     }
 }
