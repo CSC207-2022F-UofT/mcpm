02953ee6efc8aab99f763b2c75805cfa61db7732
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-11-01 22:37:12 -0400

    [+] Create collector for pair

--- src/main/java/org/hydev/mcpm/utils/Pair.java
+++ src/main/java/org/hydev/mcpm/utils/Pair.java
@@ -1,11 +1,45 @@
 package org.hydev.mcpm.utils;
 
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 /**
- * Simple generic tuple
+ * Simple unmodifiable generic pair
  *
  * @author Azalea (https://github.com/hykilpikonna)
  * @since 2022-11-01
  */
-public record Pair<K, V>(K k, V v)
+public record Pair<K, V>(K k, V v) implements Map.Entry<K, V>
 {
+    public static class UnmodifiableException extends RuntimeException {}
+
+    @Override
+    public K getKey()
+    {
+        return k;
+    }
+
+    @Override
+    public V getValue()
+    {
+        return v;
+    }
+
+    @Override
+    public V setValue(V v)
+    {
+        // Cannot be modified
+        throw new UnmodifiableException();
+    }
+
+    /**
+     * Collect a stream of pairs to a map
+     *
+     * @return Collector
+     */
+    public static <K, V> Collector<Map.Entry<K, V>, ?, Map<K, V>> toMap()
+    {
+        return Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue);
+    }
 }
