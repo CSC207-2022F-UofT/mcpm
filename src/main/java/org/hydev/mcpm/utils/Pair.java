351f533c25ef9edc933074a511a1c2997d64bc28
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-11-01 22:09:02 -0400

    [+] Create simple genetic pair

---
+++ src/main/java/org/hydev/mcpm/utils/Pair.java
@@ -0,0 +1,11 @@
+package org.hydev.mcpm.utils;
+
+/**
+ * Simple generic tuple
+ *
+ * @author Azalea (https://github.com/hykilpikonna)
+ * @since 2022-11-01
+ */
+public record Pair<K, V>(K k, V v)
+{
+}
