5259dca31b2ee01321417c9e4cfce6cb15ac853d
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2022-11-14 00:51:47 -0500

    Add CheckForUpdates layout

---
+++ src/main/java/org/hydev/mcpm/client/database/inputs/CheckForUpdatesResult.java
@@ -0,0 +1,41 @@
+package org.hydev.mcpm.client.database.inputs;
+
+import org.hydev.mcpm.client.database.model.PluginVersionState;
+import org.hydev.mcpm.client.models.PluginModel;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Result returned from CheckForUpdatesBoundary.
+ * If a plugin is in the mismatched list, you might want to ask the user if they can be removed.
+ *
+ * @param state The outcome of the request. Must be Success for the other values to be valid.
+ * @param updatable A map from version ids to all plugins that require updates.
+ * @param mismatched A list of all plugins that were not matched to any Plugin in the database.
+ */
+public record CheckForUpdatesResult(
+    State state,
+    Map<PluginVersionState, PluginModel> updatable,
+    List<PluginVersionState> mismatched
+) {
+    /**
+     * The outcome of the CheckForUpdatesResult.
+     */
+    public enum State {
+        SUCCESS,
+        INVALID_INPUT,
+        FAILED_TO_FETCH_DATABASE,
+    }
+
+    /**
+     * Creates an empty CheckForUpdatesResult with the provided state.
+     * The default values are provided in order to easily create objects with failure States.
+     *
+     * @param state The state of the CheckForUpdatesResult.
+     * @return A CheckForUpdatesResult object with state initialized and dummy values for the other elements.
+     */
+    public static CheckForUpdatesResult by(CheckForUpdatesResult.State state) {
+        return new CheckForUpdatesResult(state, Map.of(), List.of());
+    }
+}
