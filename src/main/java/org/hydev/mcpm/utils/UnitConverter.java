62a5dca2c07037fe2f57d58277ff8864c899729b
Hykilpikonna <me@hydev.org>
2022-11-19 17:59:55 -0500

    [+] UnitConverter

--- src/main/java/org/hydev/mcpm/utils/UnitConverter.java
+++ src/main/java/org/hydev/mcpm/utils/UnitConverter.java
@@ -1,15 +1,45 @@
 package org.hydev.mcpm.utils;
 
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Unit converter utility class
+ *
+ * @author Azalea (https://github.com/hykilpikonna)
+ * @since 2022-11-19
+ */
 public class UnitConverter
 {
+    public record UnitSize(String unit, Double size) {};
+
     /**
      * Automatically selects the best unit to represent binary size.
      *
      * @param size Original size in bytes
      * @return Pair[Unit name, Converted value]
      */
-    public static Pair<String, Double> autoBinarySize(long size)
+    public static UnitSize autoBinarySize(double size)
     {
-        return null;
+        for (var unit : List.of("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"))
+        {
+            if (Math.abs(size) < 1024) return new UnitSize(unit, size);
+            size /= 1024d;
+        }
+
+        throw new AssertionError(String.format("Size %s too large for formatting", size));
+    }
+
+    /**
+     * Generate a speed formatter for unit conversion
+     *
+     * @return Speed formatter function
+     */
+    public static Function<Double, String> binarySpeedFormatter()
+    {
+        return (d) -> {
+            var size = autoBinarySize(d);
+            return String.format("%5.1f %2$3s/s", size.size(), size.unit());
+        };
     }
 }
