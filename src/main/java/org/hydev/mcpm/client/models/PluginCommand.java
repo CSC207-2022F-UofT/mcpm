b1693259c5477d3ac0026a730c66bfeab60676b7
Hykilpikonna <me@hydev.org>
2022-11-19 15:57:43 -0500

    [F] Fix command parsing

--- src/main/java/org/hydev/mcpm/client/models/PluginCommand.java
+++ src/main/java/org/hydev/mcpm/client/models/PluginCommand.java
@@ -1,26 +1,25 @@
 package org.hydev.mcpm.client.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
 /**
  * Command declared by a plugin's meta
  *
- * @param name Name to activate the command
  * @param description Description
  * @param aliases Other names
  * @param permission Required permission
- * @param permissionMessage Message sent to player when player doesn't have permissions
  * @param usage Usage help
  *
  * @author Azalea (https://github.com/hykilpikonna)
  * @since 2022-09-27
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record PluginCommand(
-    String name,
     String description,
     List<String> aliases,
     String permission,
-    String permissionMessage,
     String usage
 )
 {
