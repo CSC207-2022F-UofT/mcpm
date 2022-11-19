af3a2816354286ad6fa05d00d32c470d5b4c78fe
Azalea <me@hydev.org>
2022-11-19 17:27:09 -0500

    [+] (unfinished) unit converter

---
+++ src/main/java/org/hydev/mcpm/utils/UnitConverter.java
@@ -0,0 +1,15 @@
+package org.hydev.mcpm.utils;
+
+public class UnitConverter
+{
+    /**
+     * Automatically selects the best unit to represent binary size.
+     *
+     * @param size Original size in bytes
+     * @return Pair[Unit name, Converted value]
+     */
+    public static Pair<String, Double> autoBinarySize(long size)
+    {
+        return null;
+    }
+}
