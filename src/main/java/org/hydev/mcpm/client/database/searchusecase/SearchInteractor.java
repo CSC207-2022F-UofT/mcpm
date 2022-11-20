d067eec8597fd8d3366a67a42171b749f5ef5701
Jerry Zhu <jerryzhu509@gmail.com>
2022-11-20 16:39:51 -0500

    Refactor DatabaseInteractor, remove main method from SearchInteractor, create SearchPackagesController, and added tests for SearchInteractor.

--- src/main/java/org/hydev/mcpm/client/database/searchusecase/SearchInteractor.java
+++ src/main/java/org/hydev/mcpm/client/database/searchusecase/SearchInteractor.java
@@ -3,14 +3,10 @@ package org.hydev.mcpm.client.database.searchusecase;
 import org.hydev.mcpm.client.database.boundary.SearchPackagesBoundary;
 import org.hydev.mcpm.client.arguments.fetcher.DatabaseFetcher;
 import org.hydev.mcpm.client.arguments.fetcher.DatabaseFetcherListener;
-import org.hydev.mcpm.client.arguments.fetcher.LocalDatabaseFetcher;
 import org.hydev.mcpm.client.arguments.fetcher.ProgressBarFetcherListener;
 import org.hydev.mcpm.client.database.inputs.SearchPackagesInput;
 import org.hydev.mcpm.client.database.results.SearchPackagesResult;
 
-import java.net.URI;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * Handles searching within the database.
@@ -67,48 +63,5 @@ public class SearchInteractor implements SearchPackagesBoundary {
 
         return new SearchPackagesResult(SearchPackagesResult.State.SUCCESS,
                 SearcherFactory.createSearcher(input).getSearchList(searchStr, plugins));
-    }
-
-    /**
-     * Demo main method for DatabaseInteractor.
-     *
-     * @param args Arguments are ignored.
-     */
-    public static void main(String[] args) {
-        var host = URI.create("http://mcpm.hydev.org");
-        var fetcher = new LocalDatabaseFetcher(host);
-        var database = new SearchInteractor(fetcher);
-
-        var result = database.search(
-                new SearchPackagesInput(SearchPackagesInput.Type.BY_NAME, "SkinsRestorer", true));
-
-        if (result.state() != SearchPackagesResult.State.SUCCESS) {
-            System.out.println("Result Failed With State " + result.state().name());
-            return;
-        }
-
-        var text = result
-                .plugins()
-                .stream()
-                .map(x -> x.versions().stream().findFirst().map(value -> value.meta().name()))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .map(value -> "  " + value)
-                .collect(Collectors.joining("\n"));
-
-        System.out.println(text);
-        System.out.println();
-        
-        var result3 = database.search(new SearchPackagesInput(
-                SearchPackagesInput.Type.BY_KEYWORD, "offline online", true));
-        var text3 = result3
-                .plugins()
-                .stream()
-                .map(x -> x.versions().stream().findFirst().map(value -> value.meta().name()))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .map(value -> "  " + value)
-                .collect(Collectors.joining("\n"));
-        System.out.println(text3);
     }
 }
