1514bd5d41b86b68649fcf217d3faed1840ea53f
anushkasharma <aanushka.sharma@mail.utoronto.ca>
2022-11-20 17:38:02 -0500

    Commit attempt 5

--- src/main/java/org/hydev/mcpm/client/Downloader.java
+++ src/main/java/org/hydev/mcpm/client/Downloader.java
@@ -42,19 +42,22 @@ public class Downloader
      */
     public void downloadFile(String url, File to)
     {
+        // TODO: Implement this
+        //throw new UnsupportedOperationException("TODO");
         try (FileOutputStream fileos = new FileOutputStream(to)) {
             URL link = new URL(url);
             HttpURLConnection http = (HttpURLConnection) link.openConnection();
-            long fileSize = http.getContentLengthLong();
+            long fileSize = (long) http.getContentLengthLong();
 
 
             BufferedInputStream in = new BufferedInputStream(http.getInputStream());
             BufferedOutputStream bout = new BufferedOutputStream(fileos, 1024);
             byte[] buffer = new byte[1024];
-            int read;
+            int read = 0;
 
             var row = new ProgressRow(fileSize)
-                    .desc(format("Download %s", allRows.size()))
+                    .unit("Byte")
+                    .desc(format("Downloaded", allRows.size()))
                     .descLen(30);
             bar.appendBar(row);
             allRows.add(row);
@@ -64,6 +67,7 @@ public class Downloader
             }
             bout.close();
             in.close();
+            System.out.println("\nDownload completed");
         }
         catch (IOException ex) {
             ex.printStackTrace();
@@ -77,7 +81,7 @@ public class Downloader
      *
      * @param urls Mapping of remote urls to local file paths
      */
-    public void downloadFiles(Map<String, File> urls)
+    public void downloadFiles(Map<String, File> urls, boolean progress, int threads)
     {
         try (ExecutorService executor = Executors.newFixedThreadPool(threads))
         {
@@ -125,6 +129,36 @@ public class Downloader
         return this;
     }
 
+    class Processor implements Runnable {
+        private List<String> files;
+        private Map<String, File> urls;
+        private int id;
+
+        /**
+         * Download multiple files from the internet to local storage through HTTP requests
+         * <p>
+         * The implementation must use multithreading
+         *
+         * @param files List of remote urls
+         * @param urls Mapping of remote urls to local file paths
+         * @param id id for each file
+         */
+        public Processor(int id, Map<String, File> urls, List<String> files) {
+            this.id = id;
+            this.urls = urls;
+            this.files = files;
+        }
+
+        // Process downloading each file from multithreading
+        @Override
+        public void run() {
+            String url = files.get(id);
+            File to = urls.get(url);
+            downloadFile(url, to);
+        }
+    }
+
+
     /**
      * Displays a demo for.
      *
@@ -136,10 +170,21 @@ public class Downloader
         File out = new File("./Introduction_to_Algorithms_Third_Edition.pdf");
         String link1 = "https://www.iusb.edu/students/academic-success-programs/academic-centers-for-excellence/docs/Basic%20Math%20Review%20Card.pdf";
         File out1 = new File("./Math.pdf");
+        String link2 = "https://ouopentextbooks.org/mathematics/files/2015/07/1503.pdf";
+        File out2 = new File("./1503");
+        String link3 = "https://faculty.math.illinois.edu/~aydin/math220/lecturenotes/m220_Sec1_4.pdf";
+        File out3 = new File("./m220_Sec1_4");
+        String link4 = "https://ocw.mit.edu/ans7870/9/9.00SC/MIT9_00SCF11_text.pdf";
+        File out4 = new File("./MIT9_00SCF11_text");
+
         Downloader downloader = new Downloader();
         Map<String, File> urls = new HashMap<>();
         urls.put(link, out);
         urls.put(link1, out1);
-        downloader.downloadFiles(urls);
+        urls.put(link2, out2);
+        urls.put(link3, out3);
+        urls.put(link4, out4);
+        downloader.downloadFiles(urls, true, 2);
+
     }
 }
--- src/main/java/org/hydev/mcpm/client/Launcher.java
+++ src/main/java/org/hydev/mcpm/client/Launcher.java
@@ -1,5 +1,9 @@
 package org.hydev.mcpm.client;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * TODO: Write a description for this class!
  *
@@ -11,5 +15,17 @@ public class Launcher
     public static void main(String[] args)
     {
         System.out.println("Meow");
+        String link1 = "https://sd.blackball.lv/library/Introduction_to_Algorithms_Third_Edition_(2009).pdf";
+        File file1 = new File("/Users/anushkasharma/Documents/uni/Introduction_to_Algorithms_Third_Edition.pdf");
+        String link2 = "https://www.iusb.edu/students/academic-success-programs/academic-centers-for-excellence/docs/Basic%20Math%20Review%20Card.pdf";
+        File file2 = new File("/Users/anushkasharma/Documents/uni/Basic Math Review Card.pdf");
+
+
+        Downloader downloader = new Downloader();
+        Map<String, File> urls = new HashMap<>();
+        urls.put(link1, file1);
+        urls.put(link2, file2);
+        downloader.downloadFiles(urls, true, 2);
+
     }
 }
