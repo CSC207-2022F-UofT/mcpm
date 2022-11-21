c3a73e7b6cb40f0502c5905512a039e2272ebf2d
Azalea <me@hydev.org>
2022-11-21 14:01:45 -0500

    [T] HashUtilsTest

---
+++ src/test/java/org/hydev/mcpm/utils/HashUtilsTest.java
@@ -0,0 +1,25 @@
+package org.hydev.mcpm.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Objects;
+
+class HashUtilsTest
+{
+    HashUtils u = new HashUtils();
+
+    @Test
+    void hashString()
+    {
+        assert u.hash("hello world")
+            .equals("b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9");
+    }
+
+    @Test
+    void hashFile() throws IOException
+    {
+        assert u.hash(Objects.requireNonNull(GeneralUtils.getResourceFile("test-plugin-activelist.jar")))
+            .equals("6b51b4a80419843f522f0a612288c7f50cf405f5ab7dd9bb1d050cc6e80a725f");
+    }
+}
