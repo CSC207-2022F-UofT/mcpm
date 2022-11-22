3297964ae55bf2359eadc71374f340c9a16d77db
Hykilpikonna <me@hydev.org>
2022-11-22 15:42:03 -0500

    [+] Implement mirror controller

--- src/main/java/org/hydev/mcpm/client/commands/entries/MirrorController.java
+++ src/main/java/org/hydev/mcpm/client/commands/entries/MirrorController.java
@@ -2,7 +2,11 @@ package org.hydev.mcpm.client.commands.entries;
 
 import org.hydev.mcpm.client.database.MirrorSelectBoundary;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.function.Consumer;
+
+import static org.hydev.mcpm.utils.FormatUtils.tabulate;
 
 /**
  * Controller for the mirror selection command
@@ -13,6 +17,30 @@ import java.util.function.Consumer;
 public record MirrorController(MirrorSelectBoundary boundary)
 {
     /**
+     * Format ping with colors
+     *
+     * @param ping Ping in milliseconds
+     * @return Ping with colors
+     */
+    private static String formatPing(long ping)
+    {
+        var color = ping <= 100 ? "&a" : ping <= 200 ? "&e" : "&c";
+        return color + ping + " ms";
+    }
+
+    /**
+     * Format speed with colors
+     *
+     * @param speed Speed in mbps
+     * @return Speed with colors
+     */
+    private static String formatSpeed(long speed)
+    {
+        var color = speed >= 750 ? "&a" : speed >= 500 ? "&e" : "&c";
+        return color + speed + " mbps";
+    }
+
+    /**
      * Ask the user to select a mirror
      *
      * @param refresh If we should refresh the database
@@ -20,9 +48,20 @@ public record MirrorController(MirrorSelectBoundary boundary)
      */
     public void ping(boolean refresh, Consumer<String> log)
     {
-        System.out.println("ping");
-        // TODO: Implement this
-        throw new UnsupportedOperationException("TODO");
+        try
+        {
+            if (refresh) boundary.updateMirrors();
+
+            // Display the top 20 results
+            var ping = boundary.pingMirrors();
+            log.accept(tabulate(ping.stream().limit(20).map(it ->
+                    List.of(it.k().host(), formatPing(it.v()), formatSpeed(it.k().speed()))).toList(),
+                List.of(":Host", "Delay:", "Speed:")));
+        }
+        catch (IOException e)
+        {
+            log.accept(String.format("&cUnexpected error during processing: %s", e));
+        }
     }
 
     /**
@@ -33,8 +72,20 @@ public record MirrorController(MirrorSelectBoundary boundary)
      */
     public void sel(String host, Consumer<String> log)
     {
-        System.out.println("sel");
-        // TODO: Implement this
-        throw new UnsupportedOperationException("TODO");
+        try
+        {
+            var mirror = boundary.listAvailableMirrors().stream().filter(it -> it.host().equals(host)).findFirst()
+                .orElseThrow(() -> new AssertionError(String.format("No mirror of the host %s is found", host)));
+
+            boundary.setSelectedMirror(mirror);
+        }
+        catch (IOException e)
+        {
+            log.accept(String.format("&cError occurred while writing the configuration file: %s", e));
+        }
+        catch (AssertionError e)
+        {
+            log.accept("&c" + e.getMessage());
+        }
     }
 }
