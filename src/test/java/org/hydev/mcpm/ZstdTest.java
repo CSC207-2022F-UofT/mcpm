5124820bc2d18c5fac6115f398d72d2a36891c8a
Hykilpikonna <me@hydev.org>
2022-11-22 00:56:35 -0500

    [T] Test zstd support detection

--- src/test/java/org/hydev/mcpm/ZstdTest.java
+++ src/test/java/org/hydev/mcpm/ZstdTest.java
@@ -12,6 +12,15 @@ public class ZstdTest
 {
     public static void main(String[] args)
     {
+        var enableCompression = false;
+        try
+        {
+            Zstd.compress("Hello world".getBytes());
+            enableCompression = true;
+        }
+        catch (Exception ignored) { }
+        System.out.printf("Detected ZSTD support: %s\n", enableCompression);
+
         var cb = Zstd.compress("Zstd compression/decompression works!".getBytes());
         System.out.println(new String(Zstd.decompress(cb, (int) Zstd.decompressedSize(cb))));
     }
