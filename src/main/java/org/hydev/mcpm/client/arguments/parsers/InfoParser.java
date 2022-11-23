330a83f6db4466d25ab63037617afe982702a9e1
Hykilpikonna <me@hydev.org>
2022-11-22 22:12:00 -0500

    [+] Info parser

---
+++ src/main/java/org/hydev/mcpm/client/arguments/parsers/InfoParser.java
@@ -0,0 +1,41 @@
+package org.hydev.mcpm.client.arguments.parsers;
+
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
+import org.hydev.mcpm.client.commands.entries.InfoController;
+
+import java.util.function.Consumer;
+
+/**
+ * Command parser for the info use case
+ *
+ * @author Azalea (https://github.com/hykilpikonna)
+ * @since 2022-11-22
+ */
+public record InfoParser(InfoController controller) implements CommandParser
+{
+    @Override
+    public String name()
+    {
+        return "info";
+    }
+
+    @Override
+    public String description()
+    {
+        return "Show the info of an installed plugin";
+    }
+
+    @Override
+    public void run(Namespace details, Consumer<String> log)
+    {
+        controller.info(details.getString("name"), log);
+    }
+
+    @Override
+    public void configure(Subparser parser)
+    {
+        parser.addArgument("name")
+            .help("Name of the plugin");
+    }
+}
