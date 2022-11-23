1f6226536d0c8570dd1abff0deeb63d8bf71126e
Hykilpikonna <me@hydev.org>
2022-11-22 23:49:00 -0500

    [O] Lint magic numbers, if braces

--- src/main/java/org/hydev/mcpm/client/commands/entries/InfoController.java
+++ src/main/java/org/hydev/mcpm/client/commands/entries/InfoController.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Streams;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.WordUtils;
 import org.hydev.mcpm.client.database.PluginTracker;
+import org.hydev.mcpm.utils.ColorLogger;
 
 import javax.annotation.Nullable;
 import java.util.Arrays;
@@ -22,6 +23,9 @@ public record InfoController(PluginTracker tracker)
 {
     private static final int LEN_LEFT = 12;
     private static final int LEN_RIGHT = 60;
+    private static final String KV_PRE = "&7> &f";
+    private static final String KV_SEP = " : &6";
+    private static final int LEN_INDENT = LEN_LEFT + ColorLogger.lengthNoColor(KV_PRE + KV_SEP);
 
     /**
      * Format a kv pair if value isn't null, return empty string if value is null
@@ -34,16 +38,30 @@ public record InfoController(PluginTracker tracker)
     {
         if (rawValue == null) return "";
 
+        // If a map is passed in, only show its keys
+        if (rawValue instanceof Map<?,?> v)
+        {
+            rawValue = v.keySet().stream().toList();
+        }
+
         String value;
-        if (rawValue instanceof Map<?,?> v) rawValue = v.keySet().stream().toList();
-        if (rawValue instanceof List<?> v) value = String.join(", ", v.stream().map(Object::toString).toList());
-        else value = rawValue.toString();
+
+        if (rawValue instanceof List<?> v)
+        {
+            // If a list is passed in, join it with commas.
+            value = String.join(", ", v.stream().map(Object::toString).toList());
+        }
+        else
+        {
+            // If any other types of object is passed in, just make it string
+            value = rawValue.toString();
+        }
 
         // Wrap text
         value = String.join("\n", Arrays.stream(WordUtils.wrap(value, LEN_RIGHT).split("\n"))
-            .map(it -> StringUtils.repeat(" ", LEN_LEFT + 5) + "&6" + it).toList()).strip();
+            .map(it -> StringUtils.repeat(" ", LEN_INDENT) + "&6" + it).toList()).strip();
 
-        return "&7> &f" + StringUtils.rightPad(key, LEN_LEFT) + " : &6" + value + "&r\n";
+        return KV_PRE + StringUtils.rightPad(key, LEN_LEFT) + KV_SEP + value + "&r\n";
     }
 
     /**
