7e54c1ced1b3edac74f9cbb63c78c3ae66e8241f
Hykilpikonna <me@hydev.org>
2022-11-25 00:56:55 -0500

    [+] PluginLoaderHelper

--- src/main/java/org/hydev/mcpm/client/injector/PluginLoader.java
+++ src/main/java/org/hydev/mcpm/client/injector/PluginLoader.java
@@ -13,10 +13,19 @@ import org.bukkit.plugin.RegisteredListener;
 import org.hydev.mcpm.client.models.PluginYml;
 import org.hydev.mcpm.utils.PluginJarFile;
 
+import javax.annotation.Nullable;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
 import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
 
 import static org.hydev.mcpm.utils.ReflectionUtils.getPrivateField;
 import static org.hydev.mcpm.utils.ReflectionUtils.setPrivateField;
@@ -52,62 +61,97 @@ public class PluginLoader implements LoadBoundary, UnloadBoundary, ReloadBoundar
     @Override
     public boolean loadPlugin(File jar)
     {
+        return loadPluginHelper(jar) != null;
+    }
+
+    /**
+     * Helper for loadPlugin
+     *
+     * @param jar Jar file
+     * @return Plugin instance or null
+     */
+    static @Nullable Plugin loadPluginHelper(File jar)
+    {
         // 2. Load plugin
         var pm = Bukkit.getPluginManager();
         try
         {
             var plugin = pm.loadPlugin(jar);
-            if (plugin == null) return false;
+            if (plugin == null) return null;
 
             // 3. Call onLoad()
             plugin.onLoad();
             pm.enablePlugin(plugin);
-            return true;
+            return plugin;
         }
         catch (InvalidPluginException | InvalidDescriptionException e)
         {
             // These are errors indicating that the plugin we're trying to load is badly formatted.
             // There are nothing we can do.
             e.printStackTrace();
-            return false;
+            return null;
         }
+    }
+
+    /**
+     * Find a loaded plugin, throw error if not found
+     *
+     * @param name Plugin name (case-insensitive)
+     * @return Plugin object
+     * @throws PluginNotFoundException Not found
+     */
+    private Plugin findLoadedPlugin(String name) throws PluginNotFoundException
+    {
+        return Arrays.stream(Bukkit.getPluginManager().getPlugins())
+            .filter(p -> p.getName().equalsIgnoreCase(name)).findFirst()
+            .orElseThrow(() -> new PluginNotFoundException(name));
     }
 
     @Override
     public void unloadPlugin(String name) throws PluginNotFoundException
     {
-        var pm = Bukkit.getPluginManager();
-
         // 1. Find plugin by name
-        var plugin = Arrays.stream(pm.getPlugins()).filter(p -> p.getName().equalsIgnoreCase(name)).findFirst()
-            .orElseThrow(() -> new PluginNotFoundException(name));
+        unloadPlugin(findLoadedPlugin(name));
+    }
+
+    /**
+     * Unload plugin helper
+     *
+     * @param plugin Plugin object
+     */
+    static void unloadPlugin(Plugin plugin)
+    {
+        var pm = Bukkit.getPluginManager();
 
         // 2. Unload plugin
         pm.disablePlugin(plugin);
 
         // 3. Unregister listeners
         getPrivateField(pm, "listeners", new TypeToken<Map<Event, SortedSet<RegisteredListener>>>(){})
-            .ifPresent(listeners -> listeners.values().forEach(set -> set.removeIf(l -> l.getPlugin() == plugin)));
+            .ifPresentOrElse(listeners -> listeners.values().forEach(set -> set.removeIf(l -> l.getPlugin() == plugin)),
+                () -> System.err.println("listeners cannot be accessed"));
 
         // 4. Remove command from command map
-        getPrivateField(pm, "commandMap", new TypeToken<SimpleCommandMap>(){}).ifPresent(cmdMap ->
-            getPrivateField(pm, "knownCommands", new TypeToken<Map<String, Command>>(){}).ifPresent(cmds ->
+        getPrivateField(pm, "commandMap", new TypeToken<SimpleCommandMap>(){}).ifPresentOrElse(cmdMap ->
+            getPrivateField(cmdMap, "knownCommands", new TypeToken<Map<String, Command>>(){}).ifPresentOrElse(cmds ->
                 cmds.values().stream()
                     .filter(cmd -> cmd instanceof PluginCommand).map(cmd -> (PluginCommand) cmd)
                     .filter(cmd -> cmd.getPlugin() == plugin).toList().forEach(cmd -> {
                         cmd.unregister(cmdMap);
                         cmds.values().removeIf(cmd::equals);
-                    })
-            )
+                    }), () -> System.err.println("knownCommands cannot be accessed")
+            ), () -> System.err.println("commandMap cannot be accessed")
         );
 
         // 5. Remove plugin from list
         getPrivateField(pm, "plugins", new TypeToken<List<Plugin>>(){})
-            .ifPresent(plugins -> plugins.remove(plugin));
+            .ifPresentOrElse(plugins -> plugins.remove(plugin),
+                () -> System.err.println("plugins cannot be accessed"));
 
         // 6. Remove lookup name
         getPrivateField(pm, "lookupNames", new TypeToken<Map<String, Plugin>>(){})
-            .ifPresent(names -> names.remove(plugin.getName()));
+            .ifPresentOrElse(names -> names.remove(plugin.getName()),
+                () -> System.err.println("lookupNames cannot be accessed"));
 
         // 7. Unload Java classes so that the jar can be deleted on Windows
         if (plugin.getClass().getClassLoader() instanceof URLClassLoader cl)
