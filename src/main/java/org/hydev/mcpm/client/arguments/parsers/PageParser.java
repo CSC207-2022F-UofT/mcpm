4bcb7091d3576285f23c9117c724ac6e79adb3d1
Hykilpikonna <me@hydev.org>
2022-11-24 20:40:47 -0500

    [+] Page parser

---
+++ src/main/java/org/hydev/mcpm/client/arguments/parsers/PageParser.java
@@ -0,0 +1,46 @@
+package org.hydev.mcpm.client.arguments.parsers;
+
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
+import org.hydev.mcpm.client.commands.entries.PageController;
+
+import java.util.function.Consumer;
+
+/**
+ * Parser for the pagination command
+ *
+ * @author Azalea (https://github.com/hykilpikonna)
+ * @since 2022-11-24
+ */
+public record PageParser(PageController controller) implements CommandParser
+{
+    @Override
+    public String name()
+    {
+        return "page";
+    }
+
+    @Override
+    public String description()
+    {
+        return "";
+    }
+
+    @Override
+    public void configure(Subparser parser)
+    {
+        parser.addArgument("page").type(Integer.class).help("Number of the page you want to view");
+    }
+
+    @Override
+    public void run(Namespace details, Consumer<String> log)
+    {
+        var page = controller.formatPage(details.getInt("page"));
+        if (page == null)
+        {
+            log.accept("&cNo pages available.");
+            return;
+        }
+        log.accept(page);
+    }
+}
