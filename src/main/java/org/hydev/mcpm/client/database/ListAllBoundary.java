8d4fabaffe90df049b6aa82087ea279747ea4e1f
kchprog <piecubes@gmail.com>
2022-12-02 16:12:29 -0500

    Updated docstrings, checkstyle, made ListAllInteractor use the SuperLocalPluginTracker instead

--- src/main/java/org/hydev/mcpm/client/database/ListAllBoundary.java
+++ src/main/java/org/hydev/mcpm/client/database/ListAllBoundary.java
@@ -1,6 +1,5 @@
 package org.hydev.mcpm.client.database;
 
-
 import org.hydev.mcpm.client.models.PluginYml;
 
 import java.util.List;
@@ -14,12 +13,16 @@ import java.util.List;
 public interface ListAllBoundary {
 
     /**
-     * listAllInteractor interacts with the LocalPluginTracker to get the list of plugins, according to a specified
+     * listAllInteractor interacts with the LocalPluginTracker to get the list of
+     * plugins, according to a specified
      * parameter
      *
-     * @param parameter The parameter for the ListAll use case. 'All' denotes a request to list all manually
-     *     installed plugins, 'manual' denotes a request to list all manually installed plugins, and 'outdated' denotes
-     *     a request to list all manually installed plugins that are outdated.
+     * @param parameter The parameter for the ListAll use case. 'All' denotes a
+     *                  request to list all manually
+     *                  installed plugins, 'manual' denotes a request to list all
+     *                  manually installed plugins, and 'outdated' denotes
+     *                  a request to list all manually installed plugins that are
+     *                  outdated.
      */
     List<PluginYml> listAll(String parameter);
 }
--- src/main/java/org/hydev/mcpm/client/database/ListAllInteractor.java
+++ src/main/java/org/hydev/mcpm/client/database/ListAllInteractor.java
@@ -11,23 +11,24 @@ import java.util.List;
  * @author Kevin (https://github.com/kchprog)
  * @since 2022-11-20
  */
-public class ListAllInteractor implements ListAllBoundary
-{
-    LocalPluginTracker localPluginTracker = new LocalPluginTracker();
+public class ListAllInteractor implements ListAllBoundary {
+    SuperLocalPluginTracker localPluginTracker = new SuperLocalPluginTracker();
 
     /**
-     * listAllInteractor interacts with the LocalPluginTracker to get the list of plugins, according to a specified
+     * listAllInteractor interacts with the LocalPluginTracker to get the list of
+     * plugins, according to a specified
      * parameter
      *
-     * @param parameter The parameter for the ListAll use case. 'All' denotes a request to list all manually
-     *     installed plugins, 'manual' denotes a request to list all manually installed plugins, and 'outdated' denotes
-     *     a request to list all manually installed plugins that are outdated.
+     * @param parameter The parameter for the ListAll use case. 'All' denotes a
+     *                  request to list all manually
+     *                  installed plugins, 'manual' denotes a request to list all
+     *                  manually installed plugins, and 'outdated' denotes
+     *                  a request to list all manually installed plugins that are
+     *                  outdated.
      */
-    public List<PluginYml> listAll(String parameter)
-    {
+    public List<PluginYml> listAll(String parameter) {
         var installed = localPluginTracker.listInstalled();
-        switch (parameter)
-        {
+        switch (parameter) {
             case "all":
                 return installed;
 
