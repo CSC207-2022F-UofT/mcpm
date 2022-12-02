aed04a1ae1960b2b1e41558f23072bacb8f9e5b8
kchprog <piecubes@gmail.com>
2022-12-02 18:47:11 -0500

    Update ListController.java

--- src/main/java/org/hydev/mcpm/client/commands/controllers/ListController.java
+++ src/main/java/org/hydev/mcpm/client/commands/controllers/ListController.java
@@ -1,6 +1,7 @@
 package org.hydev.mcpm.client.commands.controllers;
 
 import org.hydev.mcpm.client.list.ListAllBoundary;
+import org.hydev.mcpm.client.updater.CheckForUpdatesBoundary;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -11,33 +12,34 @@ import static org.hydev.mcpm.client.display.presenters.Table.tabulate;
  * Controller class for the ListAll use case.
  *
  * @author Kevin Chen
-*/
-public class ListController
-{
+ */
+public class ListController {
     private final ListAllBoundary listAllBoundary;
+    private final CheckForUpdatesBoundary checkForUpdatesBoundary;
 
     /**
      * Constructor for ListAllController.
      *
      * @param listAllBoundary The boundary class for ListAllController.
      */
-    public ListController(ListAllBoundary listAllBoundary) {
+    public ListController(ListAllBoundary listAllBoundary, CheckForUpdatesBoundary checkForUpdatesBoundary) {
         this.listAllBoundary = listAllBoundary;
+        this.checkForUpdatesBoundary = checkForUpdatesBoundary;
     }
 
     /**
      * Executes the ListAll use case.
      *
      * @param parameter The parameter for the ListAll use case.
-     * @param log Logger
+     * @param log       Logger
      */
     public void listAll(String parameter, Consumer<String> log) {
-        var list = listAllBoundary.listAll(parameter);
+        var list = listAllBoundary.listAll(parameter, checkForUpdatesBoundary);
 
         // Tabulate result
-        var table = tabulate(list.stream().map(p ->
-                List.of("&a" + p.name(), "&e" + p.getFirstAuthor(), p.version())).toList(),
-            List.of(":Name", "Author", "Version:"));
+        var table = tabulate(
+                list.stream().map(p -> List.of("&a" + p.name(), "&e" + p.getFirstAuthor(), p.version())).toList(),
+                List.of(":Name", "Author", "Version:"));
 
         log.accept(table);
     }
