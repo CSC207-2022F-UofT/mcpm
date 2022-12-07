a8db35b54cfb1abf7e07f0000d07bc783a940fef
kchprog <piecubes@gmail.com>
2022-12-07 17:18:46 -0500

    cleaned up code

--- src/main/java/org/hydev/mcpm/client/commands/controllers/ListController.java
+++ src/main/java/org/hydev/mcpm/client/commands/controllers/ListController.java
@@ -2,16 +2,14 @@ package org.hydev.mcpm.client.commands.controllers;
 
 import org.hydev.mcpm.client.display.presenters.ListPresenter;
 import org.hydev.mcpm.client.list.ListAllBoundary;
-import org.hydev.mcpm.client.models.PluginYml;
+
 import org.hydev.mcpm.client.updater.CheckForUpdatesBoundary;
 import org.hydev.mcpm.client.list.ListType;
 import org.hydev.mcpm.client.list.ListResult;
-import org.hydev.mcpm.client.commands.presenters.ListResultPresenter;
 
-import java.util.List;
+
+
 import java.util.function.Consumer;
-
-import static org.hydev.mcpm.client.display.presenters.Table.tabulate;
 
 /**
  * Controller class for the ListAll use case.
@@ -43,35 +41,28 @@ public class ListController {
         ListPresenter listPresenter = new ListPresenter(log);
         ListType listType;
         switch (parameter.toLowerCase()) {
-            case "all":
-                listType = ListType.ALL;
-                break;
-            case "manual":
-                listType = ListType.MANUAL;
-                break;
-            case "automatic":
-                listType = ListType.AUTOMATIC;
-                break;
-            case "outdated":
-                listType = ListType.OUTDATED;
-                break;
-            default:
+            case "all" -> listType = ListType.ALL;
+            case "manual" -> listType = ListType.MANUAL;
+            case "automatic" -> listType = ListType.AUTOMATIC;
+            case "outdated" -> listType = ListType.OUTDATED;
+            default -> {
                 ListResult queryResult = new ListResult(null, ListResult.Type.SEARCH_INVALID_INPUT);
                 listPresenter.displayResult(queryResult);
                 return;
+            }
         }
 
         try {
             var list = listAllBoundary.listAll(listType);
 
             // if list is empty
+            ListResult queryResult;
             if (list.isEmpty()) {
-                ListResult queryResult = new ListResult(list, ListResult.Type.SUCCESS_RETRIEVING_BUT_NO_MATCHES);
-                listPresenter.displayResult(queryResult);
+                queryResult = new ListResult(list, ListResult.Type.SUCCESS_RETRIEVING_BUT_NO_MATCHES);
             } else {
-                ListResult queryResult = new ListResult(list, ListResult.Type.SUCCESS_RETRIEVING_LOCAL_AND_UPDATABLE);
-                listPresenter.displayResult(queryResult);
+                queryResult = new ListResult(list, ListResult.Type.SUCCESS_RETRIEVING_LOCAL_AND_UPDATABLE);
             }
+            listPresenter.displayResult(queryResult);
         } catch (Exception e) {
             ListResult queryResult = new ListResult(null, ListResult.Type.SEARCH_FAILED_TO_FETCH_INSTALLED);
             listPresenter.displayResult(queryResult);
