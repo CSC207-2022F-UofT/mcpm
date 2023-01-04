7068ac0d8e875c9bcd1b5c94d22dc1be01db564c
Azalea Gui <me@hydev.org>
2023-01-04 00:08:02 -0500

    [F] Put fmt outside of class scope

--- src/main/java/org/hydev/mcpm/client/installer/PackageChange.kt
+++ src/main/java/org/hydev/mcpm/client/installer/PackageChange.kt
@@ -28,22 +28,22 @@ class PackageChange(
      * Compute type
      */
     val type get() = if (new != null) if (original != null) Type.UPGRADE else Type.ADD else Type.REMOVE
-
-    /**
-     * Format package change list
-     */
-    val List<PackageChange>.table get() = Table(listOf("Plugin", "Current", "Latest", "Size"), map {
-        val ty = it.type
-        listOf(
-            ty.symbol + (it.new?.meta?.name ?: it.original?.name ?: it.originalFile?.name),
-            it.original?.version ?: "&7-",
-            it.new?.meta?.version ?: "&7-",
-            it.originalFile?.length()?.sizeFmt().toString()
-        )
-    })
-
-    val List<PackageChange>.downloadSize get() = sumOf { it.new?.size ?: 0 }
-
-    val List<PackageChange>.fmt get() = "Found $size plugins changes\n\n" +
-        "$table\n\nTotal download size: $downloadSize"
 }
+
+/**
+ * Format package change list
+ */
+val List<PackageChange>.table get() = Table(listOf("Plugin", "Current", "Latest", "Size"), map {
+    val ty = it.type
+    listOf(
+        ty.symbol + (it.new?.meta?.name ?: it.original?.name ?: it.originalFile?.name),
+        it.original?.version ?: "&7-",
+        it.new?.meta?.version ?: "&7-",
+        it.originalFile?.length()?.sizeFmt().toString()
+    )
+})
+
+val List<PackageChange>.downloadSize get() = sumOf { it.new?.size ?: 0 }
+
+val List<PackageChange>.fmt get() = "Found $size plugins changes\n\n" +
+    "$table\n\nTotal download size: $downloadSize"
