3b86aa7571e5ae1eea68e87867b93eb2878dc9b0
Azalea Gui <me@hydev.org>
2023-01-04 00:58:10 -0500

    [O] Redesign install input

--- src/main/java/org/hydev/mcpm/client/installer/input/InstallInput.java
+++ src/main/java/org/hydev/mcpm/client/installer/input/InstallInput.java
@@ -1,19 +1,21 @@
 package org.hydev.mcpm.client.installer.input;
 
-import org.hydev.mcpm.client.search.SearchPackagesType;
+import java.util.List;
 
 /**
  * Install Plugin Input
  *
- * @param name     Plugin name from repository
- * @param type     Search packages type (BY_NAME, BY_KEYWORD, BY_COMMAND)
- * @param load     Whether to load after installing
+ * @param name Plugin name from repository
+ * @param type Search packages type (BY_NAME, BY_KEYWORD, BY_COMMAND)
+ * @param load Whether to load after installing
  * @param isManuallyInstalled Whether the user already asked installing the plugin
  */
-public record InstallInput(String name,
-         SearchPackagesType type,
-         boolean load,
-         boolean isManuallyInstalled) {
+public record InstallInput(
+    List<String> names,
+    List<Long> ids,
+    boolean load,
+    boolean isManuallyInstalled)
+{
 
 }
 
